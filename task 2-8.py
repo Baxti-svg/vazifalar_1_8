7a09563f8856252aea294ab85dc5d4c38f92f916
Baxti-svg <rahmonovbahtier075@gmail.com>
2025-04-24 19:14:58 +0500

    Add files via upload

---
+++ task 2-8.py
@@ -0,0 +1,71 @@
+# **************2-dars****************
+#--------> 1 misol
+#print ('Ertalabdan salom!')
+
+#--------> 2 misol
+#print(3**3)
+
+# **************3-dars****************
+#--------> 1 misol
+#print(" \"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas")
+
+#--------> 2 misol
+#print("Tomonlari 25 ga teng kvadratning yuzi", 25*25, "ga, perimetri", 4*25, "ga teng" )
+
+# **************4-dars****************
+#--------> 1 misol
+#var = "Salom dunyo!"
+#print(var)
+
+#--------> 2 misol
+#xabar = "Pythonni o'rganish kerak"
+#print(xabar)
+
+# **************5-dars****************
+#--------> 1 misol
+# kucha = "A.Temur"
+# maxalla = "Yangiyer"
+# tuman = "Qumqo'rg'on"
+# viloyat = "Surxondaryo"
+#print(f"{kucha} ko'chasi, {maxalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+#--------> 2 misol
+# adres = f"{kucha} ko'chasi, {maxalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+# print(adres)
+
+# **************6-dars****************
+#--------> 1 misol
+# yosh = int(input("yoshingiz nechida? \n>>>"))
+# t_yil = 2025-yosh
+# print("siz", t_yil, " da tugilgansiz")
+
+#--------> 2 misol
+# x = float(input("birinchi sonni kiriting: "))
+# y = float(input("ikkinchi sonni kiriting: "))
+# print(f"{x}+{y}=", x+y)
+# print(f"{x}-{y}=", x-y)
+# print(f"{x}*{y}=", x*y)
+# print(f"{x}/{y}=", x/y)
+
+# **************7-dars****************
+#--------> 1 misol
+# sonlar = [25, -43, 57.9, 2019_16]
+# print(sonlar)
+
+#--------> 2 misol
+# duslar = ['Otabek', 'Tuychi', 'Eldor', 'Zokir', 'Voxid']
+# duslar.append( 'Tura')
+# duslar.insert(0, 'Zokir')
+# duslar.insert(4, 'Ali')
+# print(duslar)
+
+# **************8-dars****************
+#--------> 1 misol
+davlatlar = ["Uzbekistan", "Qozogiston", "Singapur", "Italiya", "AQSh", "Braziliya"]
+# davlatlar.sort()
+# print("sorted() qaytargan ruyxat: ", sorted(davlatlar))
+
+#--------> 2 misol
+print(len(davlatlar))
+davlatlar.remove('AQSh')
+print(davlatlar)
